864672ea7bcd5d12cd3e08d70a53edc0fadbc42c
regder <fagren.sasha@gmail.com>
2022-06-25 09:03:26 +0300

    Добавьте файлы проекта.

---
+++ K2/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace K2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.OrderPanel = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.orderbtn = new System.Windows.Forms.Button();
+            this.ProdPanel = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.prodbtn = new System.Windows.Forms.Button();
+            this.servicePanel = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.servicebtn = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.OrderPanel.SuspendLayout();
+            this.ProdPanel.SuspendLayout();
+            this.servicePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.OrderPanel);
+            this.panel1.Controls.Add(this.orderbtn);
+            this.panel1.Controls.Add(this.ProdPanel);
+            this.panel1.Controls.Add(this.prodbtn);
+            this.panel1.Controls.Add(this.servicePanel);
+            this.panel1.Controls.Add(this.servicebtn);
+            this.panel1.Controls.Add(this.panelLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 874);
+            this.panel1.TabIndex = 0;
+            // 
+            // OrderPanel
+            // 
+            this.OrderPanel.Controls.Add(this.button10);
+            this.OrderPanel.Controls.Add(this.button9);
+            this.OrderPanel.Controls.Add(this.button8);
+            this.OrderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OrderPanel.Location = new System.Drawing.Point(0, 486);
+            this.OrderPanel.Name = "OrderPanel";
+            this.OrderPanel.Size = new System.Drawing.Size(220, 110);
+            this.OrderPanel.TabIndex = 1;
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.Location = new System.Drawing.Point(0, 72);
+            this.button10.Name = "button10";
+            this.button10.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button10.Size = new System.Drawing.Size(220, 36);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "button10";
+            this.button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.Location = new System.Drawing.Point(0, 36);
+            this.button9.Name = "button9";
+            this.button9.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button9.Size = new System.Drawing.Size(220, 36);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "button9";
+            this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.Location = new System.Drawing.Point(0, 0);
+            this.button8.Name = "button8";
+            this.button8.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button8.Size = new System.Drawing.Size(220, 36);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "button8";
+            this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // orderbtn
+            // 
+            this.orderbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.orderbtn.Location = new System.Drawing.Point(0, 430);
+            this.orderbtn.Name = "orderbtn";
+            this.orderbtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.orderbtn.Size = new System.Drawing.Size(220, 56);
+            this.orderbtn.TabIndex = 5;
+            this.orderbtn.Text = "Заказы";
+            this.orderbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.orderbtn.UseVisualStyleBackColor = true;
+            this.orderbtn.Click += new System.EventHandler(this.orderbtn_Click);
+            // 
+            // ProdPanel
+            // 
+            this.ProdPanel.Controls.Add(this.button6);
+            this.ProdPanel.Controls.Add(this.button5);
+            this.ProdPanel.Controls.Add(this.button1);
+            this.ProdPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ProdPanel.Location = new System.Drawing.Point(0, 320);
+            this.ProdPanel.Name = "ProdPanel";
+            this.ProdPanel.Size = new System.Drawing.Size(220, 110);
+            this.ProdPanel.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.Location = new System.Drawing.Point(0, 72);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button6.Size = new System.Drawing.Size(220, 36);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "button6";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 36);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(220, 36);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "button5";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(220, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // prodbtn
+            // 
+            this.prodbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.prodbtn.Location = new System.Drawing.Point(0, 264);
+            this.prodbtn.Name = "prodbtn";
+            this.prodbtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.prodbtn.Size = new System.Drawing.Size(220, 56);
+            this.prodbtn.TabIndex = 4;
+            this.prodbtn.Text = "Товары";
+            this.prodbtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.prodbtn.UseVisualStyleBackColor = true;
+            this.prodbtn.Click += new System.EventHandler(this.prodbtn_Click);
+            // 
+            // servicePanel
+            // 
+            this.servicePanel.Controls.Add(this.button4);
+            this.servicePanel.Controls.Add(this.button3);
+            this.servicePanel.Controls.Add(this.button2);
+            this.servicePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.servicePanel.Location = new System.Drawing.Point(0, 156);
+            this.servicePanel.Name = "servicePanel";
+            this.servicePanel.Size = new System.Drawing.Size(220, 108);
+            this.servicePanel.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 72);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(220, 35);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "button4";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 36);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(220, 36);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(220, 36);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // servicebtn
+            // 
+            this.servicebtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.servicebtn.Location = new System.Drawing.Point(0, 100);
+            this.servicebtn.Name = "servicebtn";
+            this.servicebtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.servicebtn.Size = new System.Drawing.Size(220, 56);
+            this.servicebtn.TabIndex = 2;
+            this.servicebtn.Text = "Услуги";
+            this.servicebtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.servicebtn.UseVisualStyleBackColor = true;
+            this.servicebtn.Click += new System.EventHandler(this.servicebtn_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.White;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 100);
+            this.panelLogo.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(220, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(714, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.Location = new System.Drawing.Point(0, 596);
+            this.button7.Name = "button7";
+            this.button7.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button7.Size = new System.Drawing.Size(220, 56);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Заказы";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 874);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.OrderPanel.ResumeLayout(false);
+            this.ProdPanel.ResumeLayout(false);
+            this.servicePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel servicePanel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button servicebtn;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button prodbtn;
+        private System.Windows.Forms.Panel OrderPanel;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button orderbtn;
+        private System.Windows.Forms.Panel ProdPanel;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ K2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            customDesing();
+        }
+        private void customDesing()
+        {
+            OrderPanel.Visible = false;
+            servicePanel.Visible = false;
+            ProdPanel.Visible = false;
+        }
+        private void HideSubMenu()
+        {
+            if(OrderPanel.Visible == true)            
+                OrderPanel.Visible = false;
+            if (ProdPanel.Visible == true)
+                ProdPanel.Visible = false;
+            if (servicePanel.Visible == true)
+                servicePanel.Visible = false;
+        }
+        private void ShowSubMenu(Panel submenu)
+        {
+            if(submenu.Visible == false)
+            {
+                HideSubMenu();
+                submenu.Visible = true;
+            }
+            else
+            {
+                submenu.Visible = false;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void servicebtn_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(servicePanel);
+        }
+
+        private void prodbtn_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(ProdPanel);
+        }
+
+        private void orderbtn_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(OrderPanel);
+        }
+    }
+}
